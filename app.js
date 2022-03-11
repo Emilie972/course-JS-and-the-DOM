329740a9bddb30348e98f0e2e252d9d350abcfcb
Emilie972 <95442305+Emilie972@users.noreply.github.com>
2022-03-10 18:06:27 -0800

    Display event object

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ document.addEventListener('click', function(){
 	const heading = document.querySelector('h6');
 	heading.style.backgroundColor = 'yellow';
 });
+
+//Event object
+document.addEventListener('click', function(event){
+	console.log(event);
+});
